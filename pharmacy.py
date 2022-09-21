bd9289b4714b16261dc8a04e4dd75778690a28b9
Micaela <micavera2010@hotmail.com>
2022-09-21 13:43:36 -0300

    fix file

--- pharmacy.py
+++ pharmacy.py
@@ -10,8 +10,8 @@ r = {}
 constraints = []
 
 def readFiles():
-    #farmacia = input("Ingrese el nombre del archivo y su extensión. Por ejemplo: farma01.in.\n")
-    #remedio = input("Ingrese el nombre del archivo del nuevo remedio y su extensión. Por ejemplo: remedio01.in.\n")
+    #farmacia = input("Ingrese el nombre del archivo y su extensión. Por ejemplo: farma01.in\n")
+    #remedio = input("Ingrese el nombre del archivo del nuevo remedio y su extensión. Por ejemplo: remedio01.in\n")
     farmacia = "farma03.in"
     remedio = "remedio03.in"
     Coctel(farmacia, remedio)
